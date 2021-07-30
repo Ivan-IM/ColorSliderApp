4f7e785eb282db4a6e54a78484385a2d35bd3ff0
iMarishin <move_up@icloud.com>
2021-07-30 09:29:21 +0500

    Initial Commit

---
+++ ColorSliderApp/ColorSliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderAppApp.swift
+//  ColorSliderApp
+//
+//  Created by Иван Маришин on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
